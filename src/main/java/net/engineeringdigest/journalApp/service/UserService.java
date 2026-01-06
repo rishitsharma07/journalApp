d51adae58877faa4dee5b0a7a788c8f6a4fe8bd9
rishitsharma07 <rishitsharma1675@gmail.com>
2026-01-07 01:13:27 +0530

    feat: add Spring Security authentication and protect user endpoints

--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -7,21 +7,27 @@ import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public class UserService {
 
-
     @Autowired
     private UserRepository userRepository;
 
+    private static final PasswordEncoder passwordEncoder =new BCryptPasswordEncoder();
+
     public void saveEntry(User user){
-            userRepository.save(user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
+        userRepository.save(user);
     }
 
     public List<User> getAll(){
